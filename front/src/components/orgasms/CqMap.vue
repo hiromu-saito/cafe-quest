3b3aaa2a953f935f8d43f46ba49450a649a144da
hiromu_saito <hiromu.saito@bigtreetc.com>
2022-03-21 22:47:36 +0900

    add:google map loading

--- front/src/components/orgasms/CqMap.vue
+++ front/src/components/orgasms/CqMap.vue
@@ -1,14 +1,50 @@
 <template>
-  <div>
+  <section class="bg-red mx-auto max-w-3xl">
     MapPage
-  </div>
+    <div class="map" ref="googleMap">
+    </div>
+  </section>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script>
+import { defineComponent, ref, onMounted} from 'vue'
+import GoogleMapsApiLoader from 'google-maps-api-loader'
+const apiKey = process.env.VUE_APP_GOOGLE_MAP_API_KEY
 
 export default defineComponent({
   name: 'CqMap',
+  setup(){
+    //TODO ブラウザの現在位置の情報を取得する
+    const mapConfig = {
+      center: {
+        lat: 35.68944,
+        lng: 139.69167
+      },
+      zoom: 17
+    }
+    let google
+    const googleMap = ref(null)
+
+    const initializeMap = ()=>{
+      new google.maps.Map(googleMap.value, mapConfig)
+    }
+
+    onMounted(async() => {
+      console.log(`apiKey:${apiKey}`)
+      google = await GoogleMapsApiLoader({
+        apiKey: apiKey
+      })
+      initializeMap()
+    })
+    return {
+      googleMap
+    }
+  },
 })
 
 </script>
+<style scoped>
+.map {
+  width: 800px;
+  height: 500px}
+</style>
