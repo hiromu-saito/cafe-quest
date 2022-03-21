980275a1f29a8e956160edd2580b7aca2ce61653
hiromu_saito <hiromu.saito@bigtreetc.com>
2022-03-21 21:34:13 +0900

    WIP:Time Line Page

---
+++ front/src/components/moleclues/CqPostCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col items-start mx-auto mb-8">
+    <img class="object-cover w-3/4 mb-2 overflow-hidden rounded-lg shadow-sm mx-h-25" src="https://cdn.devdojo.com/images/may2021/workout.jpg">
+    <h2 class="text-lg font-bold sm:text-xl md:text-2xl">{{post.storeName}}</h2>
+    <p class="text-md text-gray-500">{{post.impression}}</p>
+    <p class="pt-2 text-xs font-medium">
+        <a href="#_" class="mr-1 underline">{{post.userId}}</a> · 
+        <span class="mx-1">{{post.postedDate}}</span></p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface Post {
+  storeName: string
+  impression:string
+  userId :string
+  postedDate:Date
+}
+
+export default defineComponent({
+  name: 'CqPostCard',
+  props: {
+    post: {
+      type: Object as PropType<Post>
+    }
+  }
+})
+
+</script>
--- front/src/components/orgasms/CqTimeLine.vue
+++ front/src/components/orgasms/CqTimeLine.vue
@@ -1,14 +1,36 @@
 <template>
-  <div>
-    TimeLine
-  </div>
+  <section class="bg-red mx-auto max-w-3xl">
+    <cq-post-card :post="post"></cq-post-card>
+    <cq-post-card :post="post"></cq-post-card>
+    <cq-post-card :post="post"></cq-post-card>
+    <cq-post-card :post="post"></cq-post-card>
+    <cq-post-card :post="post"></cq-post-card>
+    <cq-post-card :post="post"></cq-post-card>
+    <cq-post-card :post="post"></cq-post-card>
+    <cq-post-card :post="post"></cq-post-card>
+  </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
+import CqPostCard from '../moleclues/CqPostCard.vue'
 
 export default defineComponent({
   name: 'CqTimeLine',
+  components: {
+    CqPostCard
+  },
+  setup(){
+    const post = reactive({
+      storeName: '店名',
+      impression: '感想',
+      userId: 'userID',
+      postedDate: new Date().toLocaleString('ja-JP')
+    })
+    return {
+      post
+    }
+  }
 })
 
 </script>
