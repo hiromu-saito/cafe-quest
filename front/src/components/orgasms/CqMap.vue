e8011bdd594e66a758660a1ab0120025023b1e5b
hiromu_saito <hiromu.saito@bigtreetc.com>
2022-03-22 23:50:16 +0900

    add get currentPositionMap

--- front/src/components/orgasms/CqMap.vue
+++ front/src/components/orgasms/CqMap.vue
@@ -1,6 +1,17 @@
 <template>
   <section class="bg-red mx-auto max-w-3xl">
-    MapPage
+    <div class="h-12 text-center flex justify-around">
+      <a href=""
+        class="hover:underline"
+        @click.prevent="searchFromCurrentPosition()">
+          現在地周辺から探す
+      </a>
+      <a href=""
+        @click.prevent="searchFromAddress()"
+        class="hover:underline">
+          家の近くから探す
+      </a>
+    </div>
     <div class="map" ref="googleMap">
     </div>
   </section>
@@ -14,30 +25,54 @@ const apiKey = process.env.VUE_APP_GOOGLE_MAP_API_KEY
 export default defineComponent({
   name: 'CqMap',
   setup(){
-    //TODO ブラウザの現在位置の情報を取得する
+    let google
+    const googleMap = ref(null)
     const mapConfig = {
       center: {
-        lat: 35.68944,
-        lng: 139.69167
+        lat: 0,
+        lng: 0
       },
       zoom: 17
     }
-    let google
-    const googleMap = ref(null)
 
-    const initializeMap = ()=>{
-      new google.maps.Map(googleMap.value, mapConfig)
+    const initializeMap = (lat, lng)=>{
+      mapConfig.center.lat = lat
+      mapConfig.center.lng = lng
+      const map = new google.maps.Map(googleMap.value, mapConfig)
+
+      new google.maps.Marker({
+        position: mapConfig.center,
+        map: map
+      })
     }
 
-    onMounted(async() => {
-      console.log(`apiKey:${apiKey}`)
-      google = await GoogleMapsApiLoader({
-        apiKey: apiKey
+    onMounted(() => searchFromCurrentPosition())
+
+    const searchFromCurrentPosition = () => {
+      navigator.geolocation.getCurrentPosition(async pos => {
+        const lat = pos.coords.latitude
+        const lag = pos.coords.longitude
+        google = await GoogleMapsApiLoader({
+          apiKey: apiKey
+        })
+        initializeMap(lat, lag)
+      }, err => {
+        console.log(`get current position err:${err.message}`)
+        alert('位置情報の取得に失敗しました。\nブラウザの設定から位置情報へのアクセスを許可してください。')
       })
-      initializeMap()
-    })
+    }
+
+    const searchFromAddress = () =>{
+      //TODO APIからの取得値を使う
+      const addressLat = 35.64818
+      const addressLag = 139.748775
+      initializeMap(addressLat, addressLag)
+    }
+
     return {
-      googleMap
+      googleMap,
+      searchFromCurrentPosition,
+      searchFromAddress
     }
   },
 })
